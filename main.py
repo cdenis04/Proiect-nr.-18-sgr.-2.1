80647235c6d3f95c970bf5164916fa06b7f4d820
CiobanuRazvan2005 <razvan-marian.ciobanu-tanase@student.upt.ro>
2026-06-11 02:22:54 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    student1 = Student(nume="Ciobanu Razvan", email="razvan.ciobanu@student.upt.ro", grupa="2.1")
+    profesor1 = Profesor(nume="Mocofan Muguras", email="muguras.mocofan@upt.ro", departament="ETC")
+
+    e1 = Echipament(nume="Osciloscop Digital", cod_inventar="OSC-001")
+    e2 = Echipament(nume="Multimetru Fluke", cod_inventar="MULT-102")
+    e3 = Echipament(nume="Sursa Tensiune", cod_inventar="SUR-04", stare="Defect")
+    e4 = Echipament(nume="Multimetru UNI-T", cod_inventar="MULT-105")
