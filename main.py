71c9262fdb1ec6a2e89ff49b415eb7c9abaf66f9
CiobanuRazvan2005 <razvan-marian.ciobanu-tanase@student.upt.ro>
2026-06-11 02:50:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,32 @@ if __name__ == "__main__":
     e2 = Echipament(nume="Multimetru Fluke", cod_inventar="MULT-102")
     e3 = Echipament(nume="Sursa Tensiune", cod_inventar="SUR-04", stare="Defect")
     e4 = Echipament(nume="Multimetru UNI-T", cod_inventar="MULT-105")
+
+    lab= Laborator("Sisteme Electronice")
+    lab.adauga_echipament(e1)
+    lab.adauga_echipament(e2)
+    lab.adauga_echipament(e3)
+    lab.adauga_echipament(e4)
+
+    print("\n--- Inainte de sortare ---")
+    lab.afiseaza_toate_echipamentele()
+    lab.sorteaza_echipamente()
+    print("\n--- Dupa sortare alfabetica ---")
+    lab.afiseaza_toate_echipamentele()
+    print("\n--- Rezultate cautare: 'Multimetru Fluke' ---'")
+    rezultate = lab.cauta_echipament("Multimetru Fluke")
+    for r in rezultate:
+        r.afiseaza_echipament()
+
+    print("\n--- Sesiune de imprumuturi ---")
+    imprumut1 = lab.imprumuta_echipament("OSC-001",student1)
+
+    lab.imprumuta_echipament("OSC-001",profesor1)
+    lab.imprumuta_echipament("SUR-04",profesor1)
+    lab.imprumuta_echipament("ID_GRESIT",student1)
+    print("\n--- Returnare ---")
+
+    if imprumut1:
+        imprumut1.returneaza_echipament()
+
+    lab.raport_defecte_csv()
