344bcad4de8d502acb9e98230ad0f73d26b7b028
CiobanuRazvan2005 <razvan-marian.ciobanu-tanase@student.upt.ro>
2026-06-12 21:50:11 +0300

    Update aplicatie.py

--- aplicatie.py
+++ aplicatie.py
@@ -22,3 +22,9 @@ class InterfataLaborator:
 
         tk.Button(root, text="Împrumută Echipament", bg="lightblue", command=self.apasa_imprumut).pack(pady=10)
         self.incarca_date()
+
+    def incarca_date(self):
+        self.lista_echipamente.delete(0, tk.END)
+        for e in self.lab._Laborator__echipamente:
+            text_afisat = f"{e.nume} | Cod: {e.get_cod_inventar()} | Stare: {e.stare}"
+            self.lista_echipamente.insert(tk.END, text_afisat)
